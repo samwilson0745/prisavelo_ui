0824b48c348db2ed046df756e74e7ad8f5bc530c
soham <sohamdeb0745@gmail.com>
2022-08-12 20:38:44 +0530

    Initial commit

--- lib/MainPages/ChatList.dart
+++ lib/MainPages/ChatList.dart
@@ -9,47 +9,48 @@ class ChatList extends StatefulWidget {
 }
 
 class _ChatListState extends State<ChatList> {
- PanelController panelController=new PanelController();
 
- void _toggleOpen()=>panelController.isPanelOpen?panelController.close():panelController.open();
+  PanelController panelController=new PanelController();
 
- List<Color> color=[
-    Colors.greenAccent,
-    Colors.pinkAccent,
-    Colors.lightBlueAccent,
-    Colors.white
-  ];
-
- @override
+  @override
   Widget build(BuildContext context) {
     Size size=MediaQuery.of(context).size;
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         leading: IconButton(
           onPressed: (){},
-          icon: Icon(Icons.arrow_back_ios,color: Colors.white,),
+          icon: Icon(Icons.arrow_back_ios,color: Colors.white,size: size.width*0.045,),
         ),
+        toolbarHeight: 35,
         backgroundColor: Color(0xff03071e),
-        toolbarHeight: 30,
         elevation: 0.0,
       ),
+      backgroundColor: Color(0xff03071e),
       body: Container(
         width: size.width,
-        height: size.height,
-        color: Color(0xff03071e),
-        child: SlidingUpPanel(
-          controller: panelController,
-          minHeight: 40,
-          maxHeight: size.height>700?size.height/1.45:size.height/1.55,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30)
-          ),
-          panel: Container(
+        child: Column(
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: size.width*0.06,vertical: size.height*0.01),
+                    child: Text("Chat With\nyour friends!",style: TextStyle(color: Colors.white,fontSize: size.height*0.05,fontWeight: FontWeight.w800),),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: size.width*0.01),
+                    width: size.width,
+                    child:_createList(size),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: size.height*0.01),
             width: size.width,
-            height: size.height,
+            height: size.height/1.5,
             decoration: BoxDecoration(
                 color: Color(0xfffff9eb),
                 borderRadius: BorderRadius.vertical(top:Radius.circular(30))
@@ -67,10 +68,10 @@ class _ChatListState extends State<ChatList> {
                   ),
                 ),
                 Expanded(
-                  flex: 5,
+                  flex: 4,
                   child: ListView.builder(
                     shrinkWrap: true,
-                    itemCount: 5,
+                    itemCount: 3,
                     itemBuilder: (context,index){
                       return _ListItem("image", "Soham", "Yo what up sam here i would like tell you", "18:45", size,);
                     },
@@ -78,109 +79,70 @@ class _ChatListState extends State<ChatList> {
                 )
               ],
             )
-            ),
-          body: Container(
-            margin: EdgeInsets.symmetric(vertical: size.height*0.02),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: size.width*0.06,vertical: size.height*0.01),
-                  child: Text("Find People\nto connect with!",style: TextStyle(color: Colors.white,fontSize: size.height*0.05,fontWeight: FontWeight.w800),),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: size.width*0.04,vertical:size.height*0.01),
-                  child: TextField(
-                    onTap: _toggleOpen,
-                      decoration:InputDecoration(
-                        prefixIcon: Image.asset('assets/search-2-line.png'),
-                        fillColor: Colors.white,
-                        filled: true,
-                        hintText: "Search",
-                        hintStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
-                        contentPadding: EdgeInsets.all(size.height*0.02),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff03071e),width:1),
-                            borderRadius: BorderRadius.all(Radius.circular(30))
-                        ),
-                        focusedBorder:  OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff03071e),width: 1),
-                            borderRadius: BorderRadius.all(Radius.circular(30))
-                        ),
-                      )
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: size.width*0.02),
-                  width: size.width,
-                  height: size.height/1.72,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      _createList(size,color[0]),
-                      _createList(size,color[1]),
-                      _createList(size,color[2]),
-                      _createList(size,color[3]),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        )
-      ),
-    );
+        ),
 
+        ],
+      ),
+    ));
   }
-  Widget _createList(Size size,Color color){
-   return Column(
-     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-     children: [
-       _User("Sayak Ghosh", "image", size,color),
-       _User("Sayak Ghosh", "image", size,color),
-       _User("Sayak Ghosh", "image", size,color),
-       _User("Sayak Ghosh", "image", size,color),
-     ],
-   );
+  Widget _createList(Size size){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          child: Column(
+          children: [
+            CircleAvatar(
+              radius: size.height*0.04,
+              backgroundColor:Colors.white,
+      child: Image.asset('assets/search-2-line.png'),
+      // backgroundImage: NetworkImage(image),
+          ),
+    ],
+    )
+    ),
+        _User("image", size,Colors.redAccent),
+        _User("image", size,Colors.blueAccent),
+        _User("image", size,Colors.yellowAccent),
+        _User("image", size,Colors.greenAccent),
+      ],
+    );
   }
-  Widget _User(String username,String image,Size size,Color color){
-   return Container(
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-         children: [
-           CircleAvatar(
-             radius: size.height*0.04,
-             backgroundColor:color,
-             // backgroundImage: NetworkImage(image),
-           ),
-           SizedBox(height: size.height*0.01,),
-           Text(username,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)
-         ],
-       )
-   );
+  Widget _User(String image,Size size,Color color){
+    return Container(
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: size.height*0.04,
+              backgroundColor:color,
+              // backgroundImage: NetworkImage(image),
+            ),
+          ],
+        )
+    );
   }
- Widget _ListItem(String image,String name,String recentext,String time,Size size){
-   return ListTile(
-     onTap: (){},
-     contentPadding: EdgeInsets.symmetric(horizontal: size.width*0.065,vertical: size.height*0.01),
-     leading: CircleAvatar(
-       radius: size.height*0.04,
-       backgroundColor: Colors.greenAccent,
-       // backgroundImage: NetworkImage(image),
-     ),
-     minVerticalPadding: 20,
-     horizontalTitleGap: 16,
-     title: Padding(
-       padding: const EdgeInsets.only(bottom: 5),
-       child: Row(
-         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-         children: [Text(name,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),Padding(
-           padding: EdgeInsets.symmetric(horizontal: 10),
-           child: Text(time,style: TextStyle(fontSize: 12),),
-         )],
-       ),
-     ),
-     subtitle: Text(recentext,style:TextStyle(fontSize: 15,fontWeight: FontWeight.w500),overflow: TextOverflow.ellipsis,),
-   );
- }
+  Widget _ListItem(String image,String name,String recentext,String time,Size size){
+    return ListTile(
+      onTap: (){},
+      contentPadding: EdgeInsets.symmetric(horizontal: size.width*0.065,vertical: size.height*0.01),
+      leading: CircleAvatar(
+        radius: size.height*0.04,
+        backgroundColor: Colors.greenAccent,
+        // backgroundImage: NetworkImage(image),
+      ),
+      minVerticalPadding: 20,
+      horizontalTitleGap: 16,
+      title: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text(name,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(time,style: TextStyle(fontSize: 12),),
+          )],
+        ),
+      ),
+      subtitle: Text(recentext,style:TextStyle(fontSize: 15,fontWeight: FontWeight.w500),overflow: TextOverflow.ellipsis,),
+    );
+  }
 }
